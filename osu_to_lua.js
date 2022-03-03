7c2eccfead83e6517d3693813d363c8d73da2ba3
Estrol Mendex <estrolmendex@gmail.com>
2022-03-03 23:16:48 +0700

    yes

--- osu_to_lua.js
+++ osu_to_lua.js
@@ -26,13 +26,31 @@ module.export("osu_to_lua", function(osu_contents) {
     append(beatmap.difficulty.circleSize.toString())
     
     append("T")
-	for (var timing in beatmap.timingPoints) {
-		append(timing)
-	}
+	for (let i = 0; i < beatmap.timingPoints.length; i++) {
+		let data = beatmap.timingPoints[i].split(',')
+		let time = parseInt(data[0])
+		let beatLength = parseInt(data[1])
+		let inherrited = 0
+		if (beatLength <= 0) {
+			inherrited = 1
+		}
 
+		append(format("%d,%d,%d", time, inherrited, beatLength))
+	}
+	
 	append("H")
-    for (var note in beatmap.hitObjects) {
-		append(note)
+    for (let i = 0; i < beatmap.hitObjects.length; i++) {
+		let data = beatmap.hitObjects[i].split(',')
+		let pos = parseInt(data[0])
+		let lane = Math.floor(pos * beatmap.difficulty.circleSize / 512) + 1
+		let time = parseInt(data[2])
+		let is_hold = parseInt(data[3]) == 128
+		let duration = 0
+		if (is_hold) {
+			duration = parseInt(data[5])
+		}
+	
+		append(format("%d,%d,%d,%d", time, lane, is_hold ? 1 : 0, duration))
 	}
 
     return fileContents
